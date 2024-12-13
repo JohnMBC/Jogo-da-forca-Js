ecbfb8b643bc73290b4a889cf93787bb6d4a79da
Johnny Michael Borges Carvalho <johnny.michael.b.carvalho@gmail.com>
2024-12-12 22:05:12 -0300

    Add files via upload

--- Jogo da forca/script.js
+++ Jogo da forca/script.js
@@ -1,12 +1,44 @@
-const palavra = "orquidea"
+const palavras = [
+    {
+        "palavra": "carambola",
+        "dica": "fruta"
+    },
+
+    {
+        "palavra": "arroz",
+        "dica": "alimento"
+    },
+
+    {
+        "palavra": "elefante",
+        "dica": "animal"
+    },
+
+    {
+        "palavra": "basquete",
+        "dica": "esporte"
+    }
+]
+const randomIndex = Math.floor(Math.random() * palavras.length);
+const palavra = palavras[randomIndex].palavra
+const dica = palavras[randomIndex].dica
+console.log(dica)
+
+
+// const palavra = "orquidea"
 let palavraDigitada = []
 palavraDigitada = Array(palavra.length).fill("-")
 console.log(palavraDigitada)
+const maximoTentativas = 4
+let tentativaRestante = maximoTentativas
 
-// const frutas = ["banana", "laranja", "uva"]
-const letraDigitada = "u"
+
 
 function escreveForca(letraDigitada) {
+    if (verificaFinal()) {
+        return
+    }
+
     if (letraDigitada == "") {
 
         alert("Digite uma letra")
@@ -15,7 +47,16 @@ function escreveForca(letraDigitada) {
 
     letraDigitada = letraDigitada.toLowerCase()
 
+
     document.getElementById('letra').value = ""
+
+    if (palavra.indexOf(letraDigitada) == (-1)) {
+        alert("Letra não encontrada")
+        tentativaRestante--
+        console.log("Te restam", tentativaRestante, "Tentativas")
+        return
+    }
+
 
     for (let index = 0; index < palavra.length; index++) {
         if (palavra[index].toLowerCase() === letraDigitada) {
@@ -24,5 +65,27 @@ function escreveForca(letraDigitada) {
 
         }
     }
+    // console.log(tentativaRestante)
     console.log(palavraDigitada)
 }
+
+function verificaFinal() {
+    if (tentativaRestante <= 0) {
+        console.log("Vc perdeu")
+        return false
+    }
+
+    if (palavraDigitada.indexOf("_") == -1) {
+        console.log("Vocé ganhou")
+        return false
+    }
+
+    return true
+}
+
+
+
+
+
+
+
